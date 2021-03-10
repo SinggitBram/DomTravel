4c1e8b8e80e8a750b291262037d3fa3e9ed24fe3
SinggitBram <bambadom@gmail.com>
2021-03-10 17:54:49 +0700

    footer done

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import '../../App.css'
-import Cards from '../Cards'
-import HeroSection from '../HeroSection'
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import Footer from "../Footer";
+import HeroSection from "../HeroSection";
 
 function Home() {
-    return (
-        <>
-            <HeroSection/>
-            <Cards />
-        </>
-    )
+   return (
+      <>
+         <HeroSection />
+         <Cards />
+         <Footer />
+      </>
+   );
 }
 
-export default Home
+export default Home;
